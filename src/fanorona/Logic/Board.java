65e8d4de9a79958833c4fc72327594d9ec39d6a3
galbenozilio <galbenozilio@172.20.10.2>
2019-02-06 11:41:32 +0200

    First commit

---
+++ src/fanorona/Logic/Board.java
@@ -0,0 +1,51 @@
+
+package fanorona.Logic;
+
+import fanorona.Gui.GamePanel;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+
+
+public class Board 
+{
+    public static final int ROWS = 5;
+    public static final int COLS = 9;
+    public static final int SIZE = ROWS * COLS;
+    // The difference between the edges of the image and the begining of the board.
+    public static final int DIF=15;
+    //public static final int ROWDIF = 5;
+    // The difference between each cell
+    public static final int SPACE = 30;
+    public static int cellsize;
+    Player black,white;
+    Image imageBlack, imageWhite;
+    GamePanel panel;
+    
+    public Board(GamePanel panel)
+    {
+        black = new Player(0x000000000297ffffL);
+        white = new Player(0x00001ffffd280000L);
+        imageBlack = Toolkit.getDefaultToolkit().getImage("Black.png");
+        imageWhite = Toolkit.getDefaultToolkit().getImage("Red.png");
+        this.panel = panel;
+        cellsize = 36;
+    }
+
+    public void paint(Graphics gr) 
+    {
+        long mask = 1;
+        for (int i = 0; i < SIZE; i++, mask<<=1)
+        {
+            if((black.state & mask) != 0)
+            {
+                gr.drawImage(imageBlack,i%COLS*(cellsize+SPACE)+DIF,i/COLS*(cellsize+SPACE)+DIF,cellsize,cellsize,panel);
+            }
+            if((white.state & mask) != 0)
+            {
+                gr.drawImage(imageWhite,i%COLS*(cellsize+SPACE)+DIF,i/COLS*(cellsize+SPACE)+DIF,cellsize,cellsize,panel);
+            }
+        }
+    }
+
+}
---
+++ src/fanorona/Logic/Player.java
@@ -0,0 +1,14 @@
+package fanorona.Logic;
+
+
+
+public class Player 
+{
+    // white or black
+    long state; 
+    public Player(long state)
+    {
+        this.state = state;
+    }
+
+}
